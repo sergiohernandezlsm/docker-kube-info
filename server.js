95543811c67a72d8b6fcef980ad6affcb1df5e53
Sergio <sergio.hernandez@littlestarmedia.com>
2020-12-23 10:56:31 +0000

    added console.log to test push

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const bodyParser = require('body-parser');
 const app = express();
 
 let userGoal = 'Learn Docker!';
-
+console.log('test')
 app.use(
   bodyParser.urlencoded({
     extended: false,
